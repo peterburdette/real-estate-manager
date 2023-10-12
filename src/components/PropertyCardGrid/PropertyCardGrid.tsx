c0f89467f3ecd0f7cfec08e626c1832700eb0d9f
Peter Burdette <peterburdette@gmail.com>
2023-10-11 20:04:42 -0400

    added new components

---
+++ src/components/PropertyCardGrid/PropertyCardGrid.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropertyCard from '../PropertyCard/PropertyCard';
+import { useRouter } from 'next/router';
+
+const PropertyCardGrid = (props: any) => {
+  // retrieves the current route
+  const router = useRouter();
+  const currentPath = router.pathname;
+
+  return (
+    <ul
+      role="list"
+      className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+    >
+      {props.properties.map((property: any) => (
+        <PropertyCard
+          key={property.id}
+          property={property}
+          currentRoute={currentPath}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default PropertyCardGrid;
--- src/pages/manage/[id]/index.tsx
+++ src/pages/manage/[id]/index.tsx
@@ -1,3 +1,148 @@
-export default function Home() {
-  return <>[id]</>;
-}
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteProperty } from '../../../app/reducers/propertySlice';
+import { useRouter } from 'next/router';
+import useCurrencyFormatter from '@/hooks/useCurrencyFormatter';
+
+const PropertyDetail = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const { id } = router.query;
+  const properties = useSelector((state: any) => state.properties);
+
+  const property = properties.find((property: any) => property.id === id);
+
+  const deletePropertyHandler = () => {
+    dispatch(deleteProperty({ id: property[0].id }));
+    router.push('/manage');
+  };
+
+  return (
+    <>
+      <div className="max-w-screen-xl mx-auto">
+        <div className="px-4 sm:px-0">
+          <h3 className="text-base font-semibold leading-7 text-gray-900">
+            Property Details
+          </h3>
+          <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+            Personal details and application.
+          </p>
+        </div>
+        <div className="mt-6 border-t border-gray-100">
+          <dl className="divide-y divide-gray-100">
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Address
+              </dt>
+              <dd className="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                <span className="flex-grow">
+                  {property.address +
+                    ' ' +
+                    property.city +
+                    ', ' +
+                    property.state +
+                    ' ' +
+                    property.zip}
+                </span>
+                <span className="ml-4 flex-shrink-0">
+                  <button
+                    type="button"
+                    className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500"
+                  >
+                    Update
+                  </button>
+                </span>
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Availability
+              </dt>
+              <dd className="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                <span className="flex-grow">{property.availability}</span>
+                <span className="ml-4 flex-shrink-0">
+                  <button
+                    type="button"
+                    className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500"
+                  >
+                    Update
+                  </button>
+                </span>
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Amenities
+              </dt>
+              <dd className="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                <span className="flex-grow">{property.amenities}</span>
+                <span className="ml-4 flex-shrink-0">
+                  <button
+                    type="button"
+                    className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500"
+                  >
+                    Update
+                  </button>
+                </span>
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Property Value
+              </dt>
+              <dd className="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                <span className="flex-grow">
+                  ${useCurrencyFormatter(property.propertyValue)}
+                </span>
+                <span className="ml-4 flex-shrink-0">
+                  <button
+                    type="button"
+                    className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500"
+                  >
+                    Update
+                  </button>
+                </span>
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Monthly Rent Rate
+              </dt>
+              <dd className="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                <span className="flex-grow">
+                  ${useCurrencyFormatter(property.monthlyRentalIncome)}
+                </span>
+                <span className="ml-4 flex-shrink-0">
+                  <button
+                    type="button"
+                    className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500"
+                  >
+                    Update
+                  </button>
+                </span>
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Notes
+              </dt>
+              <dd className="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                <span className="flex-grow">{property.notes}</span>
+                <span className="ml-4 flex-shrink-0">
+                  <button
+                    type="button"
+                    className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500"
+                  >
+                    Update
+                  </button>
+                </span>
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PropertyDetail;
