f3d063b82ff713c10a94ac2595e41e4f2599ead6
Peter Burdette <peterburdette@gmail.com>
2023-10-13 21:22:28 -0500

    updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+import forms from '@tailwindcss/forms';
 
 const config: Config = {
   content: [
@@ -15,6 +16,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [forms],
+};
+export default config;
