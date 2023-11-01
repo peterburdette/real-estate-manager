dae5869f1e0047af50c2a39816b1eb67f587e98d
Peter Burdette <peterburdette@gmail.com>
2023-10-31 22:42:02 -0400

    updated profile photo

--- src/components/Layout/DefaultLayout.tsx
+++ src/components/Layout/DefaultLayout.tsx
@@ -394,7 +394,7 @@ const DefaultLayout = ({ children }: { children: ReactNode }) => {
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src={profile.src}
                         alt=""
                       />
                     </Menu.Button>
