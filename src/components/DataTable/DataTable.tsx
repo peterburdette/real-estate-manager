2bbfc0b8893ebd321863b047fcd191d4f3737dc3
Peter Burdette <peterburdette@gmail.com>
2024-04-08 16:07:34 -0400

    adjusted DataTable spacing

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -21,7 +21,7 @@ const data = [
 
 const DataTable = () => {
   return (
-    <div className="px-4 sm:px-6 lg:px-8">
+    <div className="px-">
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
           <h1 className="text-base font-semibold leading-6 text-gray-900">
