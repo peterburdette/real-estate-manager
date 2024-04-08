ba9f186671b58c92aae5ddb762253e9d8a2ffe2c
Peter Burdette <peterburdette@gmail.com>
2024-04-08 16:21:47 -0400

    fixed footer width

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -93,7 +93,7 @@ const Footer = () => {
       >
         Footer
       </h2>
-      <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
+      <div className="mt-6 px-4 sm:px-6 lg:px-8">
         <div className="xl:grid xl:grid-cols-3 xl:gap-8">
           <div className="space-y-8">
             <img
