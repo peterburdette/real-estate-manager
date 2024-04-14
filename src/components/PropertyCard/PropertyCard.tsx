91e913a10c043384ccc569137932428fdd891cd3
Peter Burdette <peterburdette@gmail.com>
2024-04-13 21:33:38 -0400

    fixed styling bugs

--- src/components/PropertyCard/PropertyCard.tsx
+++ src/components/PropertyCard/PropertyCard.tsx
@@ -58,7 +58,7 @@ const PropertyCard = (props: any) => {
         <div className="-mt-px flex divide-x divide-gray-200">
           <div className="flex w-0 flex-1">
             <Link
-              className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+              className={`relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900 ${styles.button}`}
               role="button"
               href={`/manage/${props.property.id}`}
             >
@@ -70,7 +70,7 @@ const PropertyCard = (props: any) => {
           </div>
           <div className="-ml-px flex w-0 flex-1">
             <span
-              className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+              className={`relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900 ${styles.button}`}
               onClick={deletePropertyHandler}
             >
               <XMarkIcon
