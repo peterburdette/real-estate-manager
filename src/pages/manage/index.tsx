26ead6a2bec6bff2d8280efa4d1ef3849c2d5cb3
Peter Burdette <peterburdette@gmail.com>
2024-04-16 14:22:12 -0400

    implemented views option on manage page

--- src/pages/manage/index.tsx
+++ src/pages/manage/index.tsx
@@ -1,11 +1,58 @@
+import React, { useState } from 'react';
 import PropertyCardGrid from '@/components/PropertyCardGrid/PropertyCardGrid';
+// import ListView from '@/components/ListView/ListView'; // Import your ListView component
 import { getProperties } from '@/server/properties/getPropertiesApi';
+import { Bars4Icon, Squares2X2Icon } from '@heroicons/react/24/solid';
 
 const Manage = ({ data }: { data: any }) => {
+  const [viewMode, setViewMode] = useState<'list' | 'grid'>('list');
+
+  const handleListViewClick = () => {
+    setViewMode('list');
+  };
+
+  const handleGridViewClick = () => {
+    setViewMode('grid');
+  };
+
   return (
     <div className="min-h-full">
-      <div className="mx-auto px-4 pb-12 sm:px-6 lg:px-8">
-        <PropertyCardGrid properties={data} />
+      <div className="mx-auto pb-12">
+        <div className="mb-4 py-4 flex items-center border-b border-gray-200">
+          <div className="items-center rounded-lg bg-gray-100 p-0.5 sm:flex">
+            <button
+              type="button"
+              className={`rounded-md p-1.5 text-gray-400 hover:bg-white hover:shadow-sm focus:outline-none ${
+                viewMode === 'list' ? 'bg-white shadow-sm' : ''
+              }`}
+              onClick={handleListViewClick}
+            >
+              <Bars4Icon
+                className="h-5 w-5"
+                aria-hidden="true"
+              />
+              <span className="sr-only">Use list view</span>
+            </button>
+            <button
+              type="button"
+              className={`ml-0.5 rounded-md p-1.5 text-gray-400 hover:bg-white hover:shadow-sm focus:outline-none ${
+                viewMode === 'grid' ? 'bg-white shadow-sm' : ''
+              }`}
+              onClick={handleGridViewClick}
+            >
+              <Squares2X2Icon
+                className="h-5 w-5"
+                aria-hidden="true"
+              />
+              <span className="sr-only">Use grid view</span>
+            </button>
+          </div>
+        </div>
+        {viewMode === 'list' ? (
+          'list view'
+        ) : (
+          <PropertyCardGrid properties={data} />
+        )}
       </div>
     </div>
   );
