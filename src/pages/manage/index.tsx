008e91c6401ed4396df326853bb77c9772647d6b
Peter Burdette <peterburdette@gmail.com>
2024-04-17 12:48:10 -0400

    added new property button

--- src/pages/manage/index.tsx
+++ src/pages/manage/index.tsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 import PropertyCardGrid from '@/components/PropertyCardGrid/PropertyCardGrid';
 // import ListView from '@/components/ListView/ListView'; // Import your ListView component
 import { getProperties } from '@/server/properties/getPropertiesApi';
-import { Bars4Icon, Squares2X2Icon } from '@heroicons/react/24/solid';
+import { Bars4Icon, PlusIcon, Squares2X2Icon } from '@heroicons/react/24/solid';
 import { getViewPropertiesToggleState } from '@/server/appState/getViewPropertiesToggleStateApi';
 import { updateViewPropertiesToggleState } from '@/server/appState/putViewPropertiesToggleStateApi';
+import { useRouter } from 'next/router';
 
 interface ViewModeState {
   _id: string;
@@ -21,6 +22,7 @@ const ManagePage = ({ data, viewModeState }: ManageProps) => {
   const [viewMode, setViewMode] = useState<'list' | 'grid'>(
     viewModeState[0].viewMode
   );
+  const router = useRouter();
 
   useEffect(() => {
     const updateDatabase = async () => {
@@ -38,10 +40,14 @@ const ManagePage = ({ data, viewModeState }: ManageProps) => {
     setViewMode(newMode);
   };
 
+  const handleAddNewProperty = () => {
+    router.push('/add-property');
+  };
+
   return (
     <div className="min-h-full">
       <div className="mx-auto pb-12">
-        <div className="mb-4 py-4 flex items-center border-b border-gray-200">
+        <div className="mb-4 py-4 flex items-center justify-between border-b border-gray-200">
           <div className="items-center rounded-lg bg-gray-100 p-0.5 sm:flex">
             <button
               type="button"
@@ -70,6 +76,17 @@ const ManagePage = ({ data, viewModeState }: ManageProps) => {
               <span className="sr-only">Use grid view</span>
             </button>
           </div>
+          <button
+            type="button"
+            className="inline-flex items-center gap-x-1.5 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+            onClick={handleAddNewProperty}
+          >
+            <PlusIcon
+              className="-ml-0.5 h-5 w-5"
+              aria-hidden="true"
+            />
+            New Property
+          </button>
         </div>
         {viewMode === 'list' ? (
           'list view'
