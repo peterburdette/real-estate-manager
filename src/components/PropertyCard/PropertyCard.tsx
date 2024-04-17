dcc3f1d0204e1889a63682ac3fb61681dcb4bfa2
Peter Burdette <peterburdette@gmail.com>
2024-04-17 09:00:11 -0400

    updated Property Card

--- src/components/PropertyCard/PropertyCard.tsx
+++ src/components/PropertyCard/PropertyCard.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 const PropertyCard = ({ property, onDelete }: any) => {
   const router = useRouter();
+  const [isHovered, setIsHovered] = useState(false);
 
   const handleViewClick = () => {
     router.push(`/manage/${property.id}`);
   };
 
   return (
-    <div className="bg-white rounded-xl shadow-md overflow-hidden sm:max-w-md sm:mx-auto md:w-full">
+    <div
+      className="bg-white rounded-xl shadow-md overflow-hidden sm:max-w-md sm:mx-auto md:w-full relative"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div className="shrink-0">
-        <Link href={`/manage/${property.id}`}>
-          <img
-            className="h-48 w-full object-cover"
-            src={`${property.image}`}
-            alt={`${property.address}`}
-          />
-        </Link>
+        <img
+          className="h-48 w-full object-cover"
+          src={`${property.image}`}
+          alt={`${property.address}`}
+        />
       </div>
       <div className="p-8">
         <div className="mb-3 tracking-wide">
@@ -32,24 +35,25 @@ const PropertyCard = ({ property, onDelete }: any) => {
             {property.availability}
           </span>
         </div>
-        <div className="block mt-1 text-sm leading-tight font-medium text-black">
+        <div className="block mt-1 text-md leading-tight font-medium text-black">
           {property.address} {property.city}, {property.state} {property.zip}
         </div>
-        <p className="mt-2 text-slate-500">
+        <p className="mt-2 text-sm text-slate-500">
           {property.bedrooms} bds {property.bathrooms} ba &mdash;{' '}
           {property.squareFeet} sqft
         </p>
-
-        <div className="mt-5">
+      </div>
+      {isHovered && (
+        <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-70 backdrop-blur-sm">
           <button
             type="button"
-            className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+            className="inline-flex items-center rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
             onClick={handleViewClick}
           >
             View Property
           </button>
         </div>
-      </div>
+      )}
     </div>
   );
 };
