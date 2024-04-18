31863154f952c99f0a4b040001938e39ac33e25b
Peter Burdette <peterburdette@gmail.com>
2024-04-18 13:59:23 -0400

    updated tab colors

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -39,7 +39,7 @@ const Tabs: React.FC<TabsProps> = ({ tabs, onTabClick, currentTab }) => {
         <select
           id="tabs"
           name="tabs"
-          className="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
+          className="block w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring-blue-500"
           value={currentTab}
           onChange={(e) => onTabClick(Number(e.target.value))}
         >
@@ -64,7 +64,7 @@ const Tabs: React.FC<TabsProps> = ({ tabs, onTabClick, currentTab }) => {
               onClick={() => onTabClick(index)}
               className={`${
                 currentTab === index
-                  ? 'bg-indigo-100 text-indigo-700'
+                  ? 'bg-blue-100 text-blue-700'
                   : 'text-gray-500 hover:text-gray-700'
               } rounded-md group inline-flex items-center px-3 py-2 text-sm font-medium`}
               aria-current={currentTab === index ? 'page' : undefined}
